3da36155bf2c7bce4f3cb74d76832fed5d7682db
yizhun <sim.yizhun@gmail.com>
2023-04-23 23:22:09 +0800

    okta redirect config to https://vttp-miniproject2.vercel.app/login/callback from https://localhost:4200/login/callback

--- client/src/app/config/my-app-config.ts
+++ client/src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa92x5fdtwNVQSYp5d7',
     issuer: 'https://dev-08807004.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:4200/login/callback',
+    redirectUri: 'https://vttp-miniproject2.vercel.app/login/callback',
     scopes: ['openid', 'profile', 'email']
 }
 }
