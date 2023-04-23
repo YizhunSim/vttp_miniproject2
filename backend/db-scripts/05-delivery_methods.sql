0b6ee5c8ef87eb44ea27c8b78c3256991cd64c2d
yizhun <sim.yizhun@gmail.com>
2023-04-23 20:55:17 +0800

    Deployment frontend to vercel

---
+++ backend/db-scripts/05-delivery_methods.sql
@@ -0,0 +1,30 @@
+USE `full-stack-ecommerce`;
+
+SET foreign_key_checks = 0;
+
+--
+-- Table structure for table `delivery_methods`
+--
+
+DROP TABLE IF EXISTS `delivery_methods`;
+
+CREATE TABLE `delivery_methods` (
+  `id` smallint unsigned NOT NULL,
+  `short_name` varchar(4) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `delivery_time` varchar(255) DEFAULT NULL,
+  `price` decimal(13,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+--
+-- Data for table `country`
+--
+
+INSERT INTO `delivery_methods` VALUES 
+(1,'UPS1','Fatest delivery time', '1-2 Days', 10),
+(2,'UPS2','Get it within 5 days', '2-5 Days', 5),
+(3,'UPS3','Slower but cheap', '5-10 Days', 2),
+(4,'FREE','Free! You get what you pay for', '1-2 Weeks', 0);
+
+SET FOREIGN_KEY_CHECKS=1;
--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  cartifyApiUrl: 'https://localhost:8443/api',
+  cartifyApiUrl: 'http://localhost:8080/api',
   stripePublishableKey: 'pk_test_51MwLxDKPLuyWklbizUlYrI4IuXsqK3yHLAOpy2KmScQuV4o7fbwpSgh9ARXg4PvFmRI0FKGAK94BmNLy7wU0leMH00NlN9v6P1',
 
 };
